b01b055ced18a2393f7243f374a3ca2116686ca8
alurue9140 <91330447+alurue9140@users.noreply.github.com>
2021-11-24 17:32:00 +0100

    Update APAGADO.cmd

--- APAGADO.cmd
+++ APAGADO.cmd
@@ -1,3 +1,4 @@
 @echo off 
 REM Scripts inicio 
-echo El servidor %computername% se ha apagado el %date% a las %time% >> C:\log\apagados.log pause
+echo El servidor %computername% se ha apagado el %date% a las %time% >> C:\log\apagados.log 
+pause
